fd54823dcbf7a86083619320daad10ddd5e0f4b7
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2022-11-22 10:05:16 +0100

    Revert "Agg prototipo passaggi"
    This reverts commit 335cc8ae5eb5e308c98d69af792bc4dafe2dcca3.

--- resources/js/Components/Custom/Navabar.vue
+++ resources/js/Components/Custom/Navabar.vue
@@ -20,7 +20,7 @@
 
             <div v-if="navbarClicked" class="absolute inset-x-0 z-20 flex-1 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center lg:justify-between" >
                 <div class="flex flex-col text-gray-600 dark:text-gray-300 lg:flex lg:px-16 lg:-mx-4 lg:flex-row lg:items-center">
-                    <a href="/selezionatratta" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">ElbaCar</a>
+                    <a href="/elbapassaggi" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">ElbaCar</a>
                     <a href="/trovacoinquilino" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">Stanze universitarie</a>
                     <a href="#" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">Offerte di lavoro</a>
                     <a href="#" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">Blog</a>
---
+++ resources/js/Pages/ElbaPassaggi.vue
@@ -0,0 +1,7 @@
+<template>
+<Lavori/>
+</template>
+<script setup>
+import Lavori from '../Components/Custom/lavori.vue';
+
+</script>

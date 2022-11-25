b38c51857c30c29b313936da73cae742a219b964
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2022-11-25 15:33:43 +0100

    Agg dropdown in seleziona tratta

---
+++ resources/js/Components/Custom/SingleCardtratta.vue
@@ -0,0 +1,19 @@
+<template>
+
+<div class="py-5">
+<div class="w-full overflow-y-auto max-w-sm overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+<img class="object-cover object-center w-full h-56" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt="avatar">
+
+<div class="flex items-center px-6 py-3 bg-gray-600">
+    <h1 class="mx-6 text-lg font-semibold text-white">Firenze</h1>
+</div>
+</div>
+</div> 
+</template>
+<script setup>
+import Dropdowntratte from './dropdowntratte.vue';
+
+
+
+
+</script>

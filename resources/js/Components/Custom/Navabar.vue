aef5fc2461d4ad063fc7518d22e03a8ff22a880b
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2023-02-06 16:03:35 +0100

    temp 6/2

--- resources/js/Components/Custom/Navabar.vue
+++ resources/js/Components/Custom/Navabar.vue
@@ -1,56 +1,96 @@
 <template>
 <Head title="AitaElba v0.1"/>
 <nav class="bg-white shadow dark:bg-gray-800 text-white">
-    <div class="container px-6 py-4 mx-auto">
-        <div class="lg:flex lg:items-center">
-            <div class="flex items-center justify-between">
-                <Link as="div" href="/login" class="pr-12">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                      </svg>
-                </Link>
-                <div>
-                    <a v-if="User" class="text-2xl font-bold text-gray-800 transition-colors duration-300 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="/dashboard">Website</a>
-                    <a v-else class="text-2xl font-bold text-gray-800 transition-colors duration-300 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="/">Sito di prova</a>
-                </div>
-                <p class="pl-12" v-if="User" >Ciao, {{User.name}}</p>
-
-                <!-- Mobile menu button -->
-                <div class="flex lg:hidden" @click="navbarClicked = !navbarClicked">
-                    <button type="button" class="dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><!-- Panino apritore -->
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
-                        </svg>
-                    </button>
-                </div>
-            </div>
-
-            <div v-if="navbarClicked" class="absolute inset-x-0 z-20 flex-1 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center lg:justify-between" >
-                <div class="flex flex-col text-gray-600 dark:text-gray-300 lg:flex lg:px-16 lg:-mx-4 lg:flex-row lg:items-center">
-                    <a href="/selezionatratta" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">ElbaCar</a>
-                    <a href="/trovacoinquilino" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">Stanze universitarie</a>
-                    <a href="#" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">Offerte di lavoro</a>
-                    <a href="/notizie" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">Notizie</a>
-                    <a href="#" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">Chi siamo</a>
-                </div>
-                
-                
-            </div>
-        </div>
+<div class="container px-6 py-4 mx-auto">
+<div class="lg:flex lg:items-center">
+<div class="flex items-center justify-between">
+    <Link as="div" href="/login" class="pr-12">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+            </svg>
+    </Link>
+    <div>
+        <a v-if="User" class="text-2xl font-bold text-gray-800 transition-colors duration-300 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="/dashboard">Accesso eseguito</a>
+        <a v-else class="text-2xl font-bold text-gray-800 transition-colors duration-300 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="/">Sito di prova</a>
     </div>
+    <p class="pl-12" v-if="User" >Ciao, {{User.name}}</p>
+    <Dropdown align="right" width="48">
+        <template #trigger>
+            <button v-if="$page.props.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                <img class="h-8 w-8 rounded-full object-cover" :src="$page.props.user.profile_photo_url" :alt="$page.props.user.name">
+            </button>
+
+            <span v-else class="inline-flex rounded-md">
+                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
+                    {{ $page.props }}
+                    
+                    <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                    </svg>
+                </button>
+            </span>
+        </template>
+
+        <template #content>
+            <!-- Account Management -->
+            <div class="block px-4 py-2 text-xs text-gray-400">
+                Manage Account
+            </div>
+
+            <DropdownLink :href="route('profile.show')">
+                Profile
+            </DropdownLink>
+
+            <DropdownLink v-if="$page.props.jetstream.hasApiFeatures" :href="route('api-tokens.index')">
+                API Tokens
+            </DropdownLink>
+
+            <div class="border-t border-gray-100" />
+
+            <!-- Authentication -->
+            <form @submit.prevent="logout">
+                <DropdownLink as="button">
+                    Log Out
+                </DropdownLink>
+            </form>
+        </template>
+    </Dropdown>
+    <!-- Mobile menu button -->
+    <div class="flex lg:hidden" @click="navbarClicked = !navbarClicked">
+        <button type="button" class="dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><!-- Panino apritore -->
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
+            </svg>
+        </button>
+    </div>
+</div>
+
+<div v-if="navbarClicked" class="absolute inset-x-0 z-20 flex-1 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center lg:justify-between" >
+    <div class="flex flex-col text-gray-600 dark:text-gray-300 lg:flex lg:px-16 lg:-mx-4 lg:flex-row lg:items-center">
+        <a href="/selezionatratta" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">ElbaCar</a>
+        <a href="/trovacoinquilino" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">Stanze universitarie</a>
+        <a href="#" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">Offerte di lavoro</a>
+        <a href="/notizie" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">Notizie</a>
+        <a href="#" class="mt-2 transition-colors duration-300 transform lg:mt-0 lg:mx-4 hover:text-gray-900 dark:hover:text-gray-200">Chi siamo</a>
+    </div>
+    
+    
+</div>
+</div>
+</div>
 </nav>
 </template>
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3';
 import { ref } from 'vue';
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
 
 const navbarClicked = ref(false);
 
 defineProps({
-    User: String
+User: String
 })
 
 </script>
-
-

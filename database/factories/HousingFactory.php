f30910b4f8d86ced126e4c19ea25ea81092207e4
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2023-02-22 09:58:57 +0100

    Working soft-delete

--- database/factories/HousingFactory.php
+++ database/factories/HousingFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -23,7 +24,7 @@ class HousingFactory extends Factory
             'city' => 'Grosseto',
             'numero_telefono' => '3885686658',
             'stato_annuncio' => 'disponibile',
-            'user_id' => '1'
+            'user_id' => User::pluck('id')->random(),
         ];
     }
 }
